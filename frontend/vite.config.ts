6c8a8c1a9371f2bbb9cb7bcd6a62d44093ee6acd
Korng12 <chanthamengkong@gmail.com>
2026-06-20 11:57:08 +0700

    edite pipeline

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // base:'/hello-world/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
